d2e5c3dc46277ee07b29e2e2cf316b4c8ff41a72
Mustenaka <mumten120@outlook.com>
2021-03-28 17:21:39 +0800

    错误码，成功码

---
+++ routes/config.py
@@ -0,0 +1,45 @@
+# 错误报错内部传递参数，作为响应数组
+errorCode = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+]
+
+# 详细错误信息
+errorCodeinfo = [
+    "You should use POST",
+    "Can not get information, please recheck the input",
+    "Wrong password or something else",
+    "can not register new account, please recheck.",
+    "You are not login in"
+]
+
+# 成功代码
+successCode = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9"
+]
+
+# 成功详细信息
+successCodeinfo = [
+    "success login",
+    "success log out",
+    "success register new account",
+    "You are already login in.",
+    "success get subject",
+    "success get chapter",
+    "success get title",
+    "success, but answer is wrong",
+    "success, and answer is right",
+    "success daily attendance"
+]
