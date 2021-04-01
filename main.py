1c0b5e10afcb586545af54f46e85f47dbc3fc1cb
Mustenaka <mumten120@outlook.com>
2021-04-01 21:12:11 +0800

    主函数

--- main.py
+++ main.py
@@ -243,9 +243,13 @@ def Register_():
 
 
 @app.route('/getChaptersall', methods=['GET', 'POST'])
-def get_chapter():
+def get_chapter_all():
     """
-    获取全部章节信息（不常用API - 测试用） 
+    获取全部章节信息，管理员查询用API
+    API: http://localhost/getChaptersall
+
+    Returns:
+        返回数据库中全部的章节信息
     """
     # GET请求 和 POST请求都可以
     try:
@@ -270,6 +274,214 @@ def get_chapter():
         })
 
 
+
+@app.route('/getTitlesall', methods=['GET', 'POST'])
+def get_title_all():
+    """
+    获取全部题目信息，管理员查询用api
+    API: http://localhost/getTitlesall
+    
+    Returns:
+        返回数据库中全部的题目信息
+    """
+    # GET请求 和 POST请求都可以
+    try:
+        # 需要先判断一次登陆状态 - 确保已经登陆才可以获取信息
+        user = session.get('user_id')
+        if not user:
+            return jsonify({
+                "error": config.errorCode[4],
+                "error_info": config.errorCodeinfo[4]
+            })
+        op = OPcontrol.OPcontrol()
+        get_dic = op.get_title_all()
+        get_dic.setdefault("success", config.successCode[6])
+        get_dic.setdefault("success_info", config.successCodeinfo[6])
+        print(get_dic)
+        return jsonify(get_dic)
+        #return jsonify(json.dumps(get_dic,f, indent = 4, separators = (',', ': ')))
+    except:
+        return jsonify({
+            "error": config.errorCode[1],
+            "error_info": config.errorCodeinfo[1]
+        })
+
+
+# 提交新题目API
+@app.route('/setnewtitle', methods=['GET', 'POST'])
+def set_new_title():
+    """
+    新增一个题目，管理员才能使用的API。
+    首先进行是否登录验证，管理员身份验证，其次进行传入信息完整性验证，随后传递给OPcontrol层进行操作
+    API: http://localhost/setnewtitle
+
+    Args:
+        user_id        # 用户ID
+        title_id       # 题目ID
+        chapters_id    # 章节ID
+        titleHead      # 题目标题
+        titleCont      # 题目内容
+        titleAnswer    # 题目答案
+        titleAnalysis  # 题目分析
+        titlespaper    # 题目出处
+        specialNote    # 特殊注解
+
+    Returns:
+        成功上传；
+        失败上传
+            原因1.题目ID提交重复
+            原因2.缺少内容
+            原因3.使用GET请求
+            原因4.传输过程有误
+    """
+    if request.method == 'POST':
+        try:
+            # 需要先判断一次登陆状态 - 确保已经登陆才可以获取信息
+            user = session.get('user_id')
+            if not user:
+                return jsonify({
+                    "error": config.errorCode[4],
+                    "error_info": config.errorCodeinfo[4]
+                })
+            # 输入筛查
+            user_id = str(request.json.get('user_id'))
+            title_id = str(request.json.get('title_id'))
+            chapters_id = str(request.json.get('chapters_id'))
+            titleHead = str(request.json.get('titleHead'))
+            titleCont = str(request.json.get('titleCont'))
+            titleAnswer = str(request.json.get('titleAnswer'))
+            titleAnalysis = str(request.json.get('titleAnalysis'))
+            titlespaper = str(request.json.get('titlespaper'))
+            specialNote = str(request.json.get('specialNote'))
+
+            li = [user_id, title_id, chapters_id, titleHead, titleCont, titleAnswer, titleAnalysis, titlespaper, specialNote]
+            if "" in li:
+                return jsonify({
+                    "error": config.errorCode[5],
+                    "error_info": config.errorCodeinfo[5] 
+                })
+
+            op = OPcontrol.OPcontrol()
+
+            # 验证该账户是否是管理员账户
+            is_administrator = op.check_administrator(user_id)
+            if is_administrator == False:
+                return jsonify({
+                    "error": config.errorCode[6],
+                    "error_info": config.errorCodeinfo[6] 
+                })
+
+            # 传递全部参数进行插入
+            is_insert_successful = op.insert_new_title(li)
+            if is_insert_successful:
+                return jsonify({
+                    "success": config.successCode[5],
+                    "success_info": config.successCodeinfo[5]
+                })
+            else:
+                return jsonify({
+                    "error": config.errorCode[7],
+                    "error_info": config.errorCodeinfo[7] 
+                })
+        except:
+            return jsonify({
+                "error": config.errorCode[1],
+                "error_info": config.errorCodeinfo[1]
+            })
+
+    else:
+        return jsonify({
+            "error": config.errorCode[0],
+            "error_info": config.errorCodeinfo[0]
+        })
+
+# 修改题目API
+@app.route('/updatetitle', methods=['GET', 'POST'])
+def update_title():
+    """
+    修改一个已经存在的，管理员才能使用的API。
+    首先进行是否登录验证，管理员身份验证，其次进行传入信息完整性验证，随后传递给OPcontrol层进行操作
+    API: http://localhost/updatetitle
+
+    Args:
+        user_id        # 用户ID
+        title_id       # 题目ID
+        chapters_id    # 章节ID
+        titleHead      # 题目标题
+        titleCont      # 题目内容
+        titleAnswer    # 题目答案
+        titleAnalysis  # 题目分析
+        titlespaper    # 题目出处
+        specialNote    # 特殊注解
+
+    Returns:
+        成功上传；
+        失败上传
+            原因1.题目ID提交重复
+            原因2.缺少内容
+            原因3.使用GET请求
+            原因4.传输过程有误
+    """
+    if request.method == 'POST':
+        try:
+            # 需要先判断一次登陆状态 - 确保已经登陆才可以获取信息
+            user = session.get('user_id')
+            if not user:
+                return jsonify({
+                    "error": config.errorCode[4],
+                    "error_info": config.errorCodeinfo[4]
+                })
+            # 输入筛查
+            user_id = str(request.json.get('user_id'))
+            title_id = str(request.json.get('title_id'))
+            chapters_id = str(request.json.get('chapters_id'))
+            titleHead = str(request.json.get('titleHead'))
+            titleCont = str(request.json.get('titleCont'))
+            titleAnswer = str(request.json.get('titleAnswer'))
+            titleAnalysis = str(request.json.get('titleAnalysis'))
+            titlespaper = str(request.json.get('titlespaper'))
+            specialNote = str(request.json.get('specialNote'))
+
+            li = [user_id, title_id, chapters_id, titleHead, titleCont, titleAnswer, titleAnalysis, titlespaper, specialNote]
+            if "" in li:
+                return jsonify({
+                    "error": config.errorCode[5],
+                    "error_info": config.errorCodeinfo[5] 
+                })
+
+            op = OPcontrol.OPcontrol()
+
+            # 验证该账户是否是管理员账户
+            is_administrator = op.check_administrator(user_id)
+            if is_administrator == False:
+                return jsonify({
+                    "error": config.errorCode[6],
+                    "error_info": config.errorCodeinfo[6] 
+                })
+
+            # 传递全部参数进行插入
+            is_update_successful = op.update_title(li)
+            if is_update_successful:
+                return jsonify({
+                    "success": config.successCode[5],
+                    "success_info": config.successCodeinfo[5]
+                })
+            else:
+                return jsonify({
+                    "error": config.errorCode[7],
+                    "error_info": config.errorCodeinfo[7] 
+                })
+        except:
+            return jsonify({
+                "error": config.errorCode[1],
+                "error_info": config.errorCodeinfo[1]
+            })
+
+    else:
+        return jsonify({
+            "error": config.errorCode[0],
+            "error_info": config.errorCodeinfo[0]
+        })
   
 # 获取全部科目信息 
 @app.route('/getsubject', methods=['GET', 'POST'])
@@ -312,7 +524,7 @@ def get_chapters_from_sub():
     API: http://localhost/getchapterfromsub
 
     Args:
-        subject_id
+        subject_id 科目ID
 
     Returns:
         科目ID，章节ID，章节名称
