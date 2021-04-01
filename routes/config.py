71b68502bb212ed3ef12481fcbb2850fae68d957
Mustenaka <mumten120@outlook.com>
2021-04-01 21:12:36 +0800

    配置项

--- routes/config.py
+++ routes/config.py
@@ -5,15 +5,21 @@ errorCode = [
     "2",
     "3",
     "4",
+    "5",
+    "6",
+    "7"
 ]
 
 # 详细错误信息
 errorCodeinfo = [
     "You should use POST",
     "Can not get information, please recheck the input",
-    "Wrong password or something else",
-    "can not register new account, please recheck your account name.",
-    "You are not login in"
+    "Wrong password or something else.",
+    "can not register new account, please recheck.",
+    "You are not login in.",
+    "Missing the necessary incoming parameters.",
+    "you are not administrator.",
+    "failed to insert new title. please recheck the title_id is repeated."
 ]
 
 # 成功代码
@@ -27,7 +33,8 @@ successCode = [
     "6",
     "7",
     "8",
-    "9"
+    "9",
+    "10",
 ]
 
 # 成功详细信息
@@ -41,5 +48,6 @@ successCodeinfo = [
     "success get title",
     "success, but answer is wrong",
     "success, and answer is right",
-    "success daily attendance"
+    "success daily attendance",
+    "success create new title",
 ]
